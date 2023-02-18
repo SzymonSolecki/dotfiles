cc92a8969feb011b3114ef54d545ff6895ab0dbf
Szymon Solecki <szymon.solecki@protonmail.com>
2023-02-18 04:34:09 +0100

    add must have plugins

---
+++ nvim/.config/nvim/lua/plugins/cmp.lua
@@ -0,0 +1,14 @@
+return {
+  "hrsh7th/nvim-cmp",
+
+  opts = function(_, opts)
+    local cmp = require("cmp")
+    opts.mapping = {
+      ["<Tab>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
+      ["<S-Tab>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
+      ["<C-Space>"] = cmp.mapping.complete(),
+      ["<C-e>"] = cmp.mapping.abort(),
+      ["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+    }
+  end,
+}
---
+++ nvim/.config/nvim/lua/plugins/comment.lua
@@ -0,0 +1,9 @@
+return {
+  "echasnovski/mini.comment",
+  opts = function(_, opts)
+    opts.mappings = {
+      comment = "<C-_>",
+      comment_line = "<C-_>",
+    }
+  end,
+}

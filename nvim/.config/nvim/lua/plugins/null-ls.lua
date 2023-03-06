4bd173033d35ecfd87ccf9f6a878be9a7c622032
Szymon Solecki <szymon.solecki@protonmail.com>
2023-03-06 23:12:03 +0100

    add refactoring code actions

--- nvim/.config/nvim/lua/plugins/null-ls.lua
+++ nvim/.config/nvim/lua/plugins/null-ls.lua
@@ -1,10 +1,15 @@
 return {
   "jose-elias-alvarez/null-ls.nvim",
 
+  dependencies = {
+    "ThePrimeagen/refactoring.nvim",
+  },
+
   opts = function()
     local nls = require("null-ls")
     return {
       sources = {
+        nls.builtins.code_actions.refactoring,
         nls.builtins.formatting.stylua,
         nls.builtins.formatting.isort,
         nls.builtins.formatting.autopep8,

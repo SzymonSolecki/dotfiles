76f93b32e5b560a989dc6a958b07d9095f563185
Szymon Solecki <szymon.solecki@protonmail.com>
2023-04-03 01:23:51 +0200

    fix tab cmp

--- nvim/.config/nvim/lua/plugins/python.lua
+++ nvim/.config/nvim/lua/plugins/python.lua
@@ -1,16 +1,18 @@
 return {
-  "neovim/nvim-lspconfig",
-  opts = {
-    servers = {
-      pyright = {
-        settings = {
-          python = {
-            analysis = {
-              autoImportCompletion = true,
-              autoSearchPaths = true,
-              diagnosticMode = "openFilesOnly",
-              typeCheckingMode = "basic",
-              useLibraryCodeForTypes = true,
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        pyright = {
+          settings = {
+            python = {
+              analysis = {
+                autoImportCompletion = true,
+                autoSearchPaths = true,
+                diagnosticMode = "openFilesOnly",
+                typeCheckingMode = "basic",
+                useLibraryCodeForTypes = true,
+              },
             },
           },
         },

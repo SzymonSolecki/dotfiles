5b5a55f7f2a34cf1324bcfd3f6d3fce033a8fa4d
Szymon Solecki <szymon.solecki@protonmail.com>
2023-04-03 01:24:09 +0200

    add basic neotest config

---
+++ nvim/.config/nvim/lua/plugins/neotest.lua
@@ -0,0 +1,21 @@
+return {
+  "nvim-neotest/neotest",
+  config = function()
+    require("neotest").setup({
+      adapters = {
+        require("neotest-python"),
+      },
+    })
+  end,
+  ft = { "python" },
+  dependencies = {
+    "nvim-neotest/neotest-python",
+  },
+
+  opts = {},
+  keys = {
+    { "<leader>tr", "<cmd>:lua require('neotest').run.run()<cr>" },
+    { "<leader>td", "<cmd>:lua require('neotest').run.run({strategy = 'dap'})<cr>" },
+    { "<leader>te", "<cmd>:lua require('neotest').summary.toggle()<cr>" },
+  },
+}

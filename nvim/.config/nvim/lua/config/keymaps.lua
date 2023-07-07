33b28822513360f3effbbf9104a2a9bde47b0dd7
Szymon Solecki <szymon.solecki@protonmail.com>
2023-07-07 22:11:45 +0200

    fix commenting after introduction of floaty terminal to lazyvim

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -6,3 +6,10 @@ vim.keymap.set("i", "jk", "<Esc>", { desc = "Escape", noremap = true, silent = t
 vim.keymap.set("i", "kj", "<Esc>", { desc = "Escape", noremap = true, silent = true })
 
 vim.keymap.set("x", "<leader>p", '"_dP', { noremap = true, silent = true })
+
+-- at some point lazyvim added floaty terminal under this keybinding
+-- and there was no way to overrite it in any other way
+vim.keymap.set("n", "<c-_>", function()
+  local mc = require("mini.comment")
+  return mc.operator() .. "_"
+end, { expr = true, desc = "Comment" })

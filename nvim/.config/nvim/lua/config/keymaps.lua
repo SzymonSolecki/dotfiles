2c5a8d1fa365fc1fd7631b3d94dd62c0fd62fba4
Szymon Solecki <szymon.solecki@protonmail.com>
2023-09-07 21:11:09 +0200

    add floating terminal on ctrl+backslash

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -1,6 +1,7 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+local Util = require("lazyvim.util")
 
 vim.keymap.set("i", "jk", "<Esc>", { desc = "Escape", noremap = true, silent = true })
 vim.keymap.set("i", "kj", "<Esc>", { desc = "Escape", noremap = true, silent = true })
@@ -13,3 +14,9 @@ vim.keymap.set("n", "<c-_>", function()
   local mc = require("mini.comment")
   return mc.operator() .. "_"
 end, { expr = true, desc = "Comment" })
+
+local lazyterm = function()
+  Util.float_term(nil, { cwd = Util.get_root() })
+end
+vim.keymap.set("n", "<C-Bslash>", lazyterm, { desc = "Terminal (root dir)" })
+vim.keymap.set("t", "<C-Bslash>", "<cmd>close<cr>", { desc = "Hide Terminal" })

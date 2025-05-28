fc11725f0a7128129a96cb01637efb5b33057604
Szymon <szymon@fedora.home>
2025-05-28 02:32:33 +0200

    use mini snippets with blink instad of nvim cmp

---
+++ .config/nvim/lua/plugins/blink.lua
@@ -0,0 +1,11 @@
+return {
+  "saghen/blink.cmp",
+  opts = {
+    signature = { enabled = true },
+    keymap = {
+      preset = "enter",
+      ["<Tab>"] = { "select_next", "fallback" },
+      ["<S-Tab>"] = { "select_prev", "fallback" },
+    },
+  },
+}

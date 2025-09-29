0396857679fed6c50170afd1f6e5dd1a3a70188c
Szymon <szymon@fedora.home>
2025-09-29 20:50:07 +0200

    new screentshot

--- .config/hypr/scripts/screenshot.sh
+++ .config/hypr/scripts/screenshot.sh
@@ -1,15 +1,25 @@
 #!/bin/bash
 
-OUTPUT_DIR="$HOME/Pictures/screenshots"
+TIMESTAMP=$(date +%Y%m%d_%H%M%S)
+TEMP_DIR="/tmp"
+TEMP_FILE="screenshot_$TIMESTAMP.png"
+TEMP_PATH="$TEMP_DIR/$TEMP_FILE"
+OUTPUT_DIR="${HOME}/Pictures/screenshots"
+ACTION_NAME="EDIT"
 
-if [[ ! -d "$OUTPUT_DIR" ]]; then
-  notify-send "Screenshot directory does not exist: $OUTPUT_DIR" -u critical -t 3000
-  exit 1
+mkdir -p "$OUTPUT_DIR"
+
+hyprshot -m region -s -o "$TEMP_DIR" -f "$TEMP_FILE" && wl-copy <"$TEMP_PATH"
+
+if [[ -f "$TEMP_PATH" ]]; then
+  ACTION=$(notify-send -w "Screenshot Taken" "Click to edit in Satty." \
+    -i "$TEMP_PATH" \
+    -u critical \
+    -A "$ACTION_NAME=Edit")
+
+  if [ "$ACTION" == "$ACTION_NAME" ]; then
+    satty --filename "$TEMP_PATH" --output-filename "$OUTPUT_DIR/screenshot_${TIMESTAMP}.png" --copy-command "wl-copy" --early-exit
+  fi
+else
+  notify-send "Screenshot Cancelled" "No screenshot taken." -t 2000
 fi
-
-pkill slurp || hyprshot -m "${1:-region}" --raw |
-  satty --filename - \
-    --output-filename "$OUTPUT_DIR/screenshot-$(date +'%Y-%m-%d_%H-%M-%S').png" \
-    --early-exit \
-    --actions-on-enter save-to-clipboard \
-    --copy-command 'wl-copy'

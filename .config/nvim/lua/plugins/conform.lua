1b19fc70f0127077407d21a94731aee24ccf5b3e
Szymon <szymon@fedora.home>
2026-04-19 16:14:32 +0200

    remove unused dap

--- .config/nvim/lua/plugins/conform.lua
+++ .config/nvim/lua/plugins/conform.lua
@@ -2,7 +2,7 @@ return {
   "stevearc/conform.nvim",
   opts = {
     formatters_by_ft = {
-      python = { "ruff_format" },
+      python = { "ruff_fix", "ruff_format", "ruff_organize_imports" },
       html = { "prettierd" },
       json = { "prettierd" },
     },
